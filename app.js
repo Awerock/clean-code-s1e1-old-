84f27042d8d0635c45c2f2696153becd70605d43
Awerock <cigan-yashka@yandex.ru>
2023-04-25 00:34:11 +0300

    fix: js input

--- app.js
+++ app.js
@@ -8,23 +8,27 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("add-item");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("task-incomplete");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("task-completed");//completed-tasks
 
 
 //New task list item
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
-
+    listItem.classList.add("todo__wrapper");
     //input (checkbox)
+
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add("todo__checkbox");
     //label
     var label=document.createElement("label");//label
+    label.classList.add("todo__lable");
     //input (text)
     var editInput=document.createElement("input");//text
+    editInput.classList.add("todo__input");
     //button.edit
     var editButton=document.createElement("button");//edit button
 
@@ -33,18 +37,21 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='todo__lable';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="todo__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.classList.add("button");
+    editButton.classList.add("button-edit");
 
-    deleteButton.className="delete";
+    deleteButton.classList.add("button");
+    deleteButton.classList.add("button-delete");
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="button__delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
